59f1f1ea8a5e30ae10b1b3de129c8d459030029b
Esteban Trujillo <144358984+EstebanAdso@users.noreply.github.com>
2025-02-18 22:55:22 -0500

    Proyecto de Veterinaria con React y ts

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts}", // Asegúrate de incluir todos los archivos JS/TS en la carpeta src
+    "./index.html"        // Asegúrate de incluir tu archivo HTML principal
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
